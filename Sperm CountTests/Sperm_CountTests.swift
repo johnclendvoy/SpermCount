fddc449ce180e5c72e6de29b2be0e277b93bebf3
John C. Lendvoy <john@node-1w7jr9ybidoarki2fnt7s96y4.ipv6.telus.net>
2016-05-19 12:39:55 -0600

    Initial Commit

---
+++ Sperm CountTests/Sperm_CountTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sperm_CountTests.swift
+//  Sperm CountTests
+//
+//  Created by John C. Lendvoy on 2016-05-19.
+//  Copyright © 2016 John C. Lendvoy. All rights reserved.
+//
+
+import XCTest
+@testable import Sperm_Count
+
+class Sperm_CountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
